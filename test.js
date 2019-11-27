bbc294f049040ccb780c839a55ddb5cc6b5b26f1
Splend1d <a24998667@gmail.com>
2019-11-27 12:22:53 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+function 
+getInputValue(){
+  // Selecting the input element and get its value 
+  var inputVal = document.getElementById("myInput").value;
+
+  // Displaying the value
+  alert(inputVal);
+}
