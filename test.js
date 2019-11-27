4f1e3e901c20a7189c513dd9f24404be9f50aa3e
Splend1d <a24998667@gmail.com>
2019-11-27 16:33:35 +0800

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-function 
-getInputValue(){
-  // Selecting the input element and get its value 
-  var inputVal = document.getElementById("myInput").value;
-
-  // Displaying the value
-  document.getElementById("errorid").innerHTML = inputVal;
-}
